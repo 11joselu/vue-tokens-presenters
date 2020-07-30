a9fecf53198b9d5ef72e7fc03ce53fc2c43748d3
Jose Cabrera <jcabrera@habitissimo.com>
2020-07-30 16:54:55 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <p>working</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss"></style>
