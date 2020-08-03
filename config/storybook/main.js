a1847d4487f3b0e6f5dbe16dbfd216d10d7e62de
Jose Cabrera <jcabrera@habitissimo.com>
2020-08-03 20:59:46 +0200

    introduce storybook documentation

---
+++ config/storybook/main.js
@@ -0,0 +1,21 @@
+module.exports = {
+  stories: ['../../src/**/*.stories.(js|jsx|ts|tsx|mdx)'],
+  addons: [
+    '@storybook/addon-actions',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        babelOptions: {
+          presets: [
+            [
+              '@vue/cli-plugin-babel/preset',
+              {
+                jsx: false,
+              },
+            ],
+          ],
+        },
+      },
+    },
+  ],
+};
